9984327b8bbcc1907abb4acd60b3bd0bbb4482d6
JesperSjostrom2 <jespersjostrom2@gmail.com>
2025-09-15 22:28:26 +0300

    changed name work >projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,7 +111,7 @@ export default function Home() {
         </div>
       </section>
       
-      {/* Work Section */}
+      {/* Projects Section */}
       <ExperienceSection />
       
       {/* Experience Section */}
